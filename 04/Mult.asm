a3ab94f4a850e3217dcd0398e7ad7fd01f8fc092
MelindaKuo <115421175+MelindaKuo@users.noreply.github.com>
2024-07-25 20:56:58 -0700

    create mult.asm p4

---
+++ 04/Mult.asm
@@ -0,0 +1,43 @@
+// R2=  R0*R1
+// can't modify R0 or R1
+
+// R2+R0
+// Loop while R1 >0 
+// R2+R0 --> R1-1
+
+
+@R0
+D=M
+
+@R2
+M=0
+
+
+(LOOP)
+
+@R1
+D=M
+
+@END
+D;JEQ
+
+@value1
+M=D
+
+@R0
+D=M
+
+
+@R2
+M=D
+
+@value1
+M=M-1
+
+@LOOP
+0;JMP
+
+
+
+
+
