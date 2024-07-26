d21fa968fdfc5354a27b79809aa23d07f9935785
MelindaKuo <115421175+MelindaKuo@users.noreply.github.com>
2024-07-25 22:28:19 -0700

    fill

--- 04/Fill.asm
+++ 04/Fill.asm
@@ -3,3 +3,46 @@
 
 
 
+
+
+(START)
+
+@KBD
+D=M
+@WHITE
+D;JEQ
+
+@color
+M=-1
+@WHITEEND
+0;JMP
+
+(WHITE)
+@color
+M=0
+(WHITEEND)
+
+@SCREEN
+D=A
+@address
+M=D
+
+(LOOP)
+
+@color
+D=M
+@address
+A=M
+M=D
+
+@address
+M=M+1
+D=M
+
+@KBD
+D=D-A
+@LOOP
+D;JNE
+
+@START
+0;JMP
