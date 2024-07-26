46e4fa94b175ec9bdd38d1c958d0d9c327d0940e
MelindaKuo <115421175+MelindaKuo@users.noreply.github.com>
2024-07-25 21:31:38 -0700

    check mult.asm

--- 04/Mult.asm
+++ 04/Mult.asm
@@ -1,46 +1,33 @@
 // R2=  R0*R1
 // can't modify R0 or R1
 
-// R2+R0
-// Loop while R1 >0 
-// R2+R0 --> R1-1
-
-
 @R0
-D=M
-
+D=M       
 @R2
-M=0
-
-
-(LOOP)
+M=0      
 
 @R1
-D=M
+D=M      // make copy of R1
+@R3
+M=D      
 
+(LOOP)
+@R3
+D=M       
 @END
-D;JEQ
-
-@value1
-M=D
+D;JEQ         // if R1 is <0 exit
 
 @R0
-D=M
-
-
+D=M       // add R0 and R2 together
 @R2
-M=M+D
+M=D+M    
 
-@value1
-M=M-1
-
+@R3
+M=M-1     // decrease 1 from R2
 @LOOP
-0;JMP
+0;JMP         // continue the loop    
 
+(END)
 @END
-0;JMP
-
-
-
-
+0;JMP     
 
