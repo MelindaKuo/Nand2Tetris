9577e1da516bd1502a63ba85fec752eb8ec29ebd
MelindaKuo <115421175+MelindaKuo@users.noreply.github.com>
2024-07-28 17:57:40 -0700

    07 vm translator so far

---
+++ 7/VMtranslator/CodeWriter.py
@@ -0,0 +1,74 @@
+
+class CodeWriter:
+
+    def __init__(self, outFile):
+        self.outFile = outFile
+        self.file = open(self.outputFile, 'w')
+
+    
+
+    def get_segment_pointer(self, segment, index):
+        if segment == "local":
+            return f'@LCL\nD=M\n@{index}\nA=D+A'
+        if segment == "argument":
+            return f'@ARG\nD=M\n@{index}\nA=D+A'
+        if segment == "this":
+            return f'@THIS\nD=M\n@{index}\nA=D+A'
+        if segment == "that":
+            return f'@THAT\nD=M\n@{index}\nA=D+A'
+        if segment == "static":
+            return f'@{self.file}'
+        if segment == "temp":
+            return f'@R{5+index}'
+        if segment == "pointer":
+            return f'@R{3+index}'
+        
+
+    
+    def push_logic(self, segment, index):
+        segment_pointer = self.get_segment_pointer(segment, index)
+        if segment == "constant":
+            return f'@{index}\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1'
+        else:
+            return segment_pointer + '\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1'
+    
+    def pop_logic(self, segment, index):
+        segment_pointer = self.get_segment_pointer(segment, index)
+        return f'@SP\nM=M-1\nA=M\nD=M\n{segment_pointer}\nM=D'
+    
+
+    def define_Arithmetic(self, operation):
+        if operation == "add":
+            return "M+D"
+        if operation == "sub":
+            return "M-D"
+        if operation == "neg":
+            return "-M"
+        if operation == "eq":
+            return "JEQ"
+        if operation == "gt":
+            return "JGT"
+        if operation == "lt":
+            return "JLT"
+        if operation == "and":
+            return "M&D"
+        if operation == "or":
+            return "M|D"
+        if operation == "not":
+            return "!M"
+        
+
+    def write_Arithmetic()
+        
+
+
+        
+
+
+
+    
+
+
+
+    def close(self):
+        self.file.close()
---
+++ 7/VMtranslator/Parser.py
@@ -0,0 +1,59 @@
+import os
+
+class Parser:
+
+    command_Arithmetic = {'add', 'sub', 'neg', 'eq', 'gt', 'lt', 'and' 'or', 'not'}
+    commands_C= {'push', 'pop', 'label', 'goto', 'if', 'function', 'return',  'call'}
+
+    def __init__(self, input_file):
+        self.input_file = input_file
+        self.commands = []
+        file_open = open(input_file, 'r')
+        for line in file_open:
+            if line.startswith('//') or line.startswith(" "):
+                continue
+            else:
+                self.commands.append(line.strip())
+        self.currInstruction = -1
+        self.currCommand = None
+        
+
+    def has_more_lines(self):
+        return self.currInstruction < len(self.commands)-1
+    
+
+    def advance(self):
+        self.currInstruction+=1
+        self.currCommand = self.commands[self.currInstruction]
+
+
+    def command_type(self):
+        if self.currCommand[0] in self.command_Arithmetic:
+            return 'C_ARITHMETIC'
+        else:
+            for c in self.commands_C:
+                if c == self.currCommand[0]:
+                    return "C_" + c.upper()
+    
+    def arg1(self):
+        if self.currCommand == "C_ARITHMETIC":
+            return self.currCommand[1:]
+        else:
+            return self.currCommand.split()[1]
+
+
+    def arg2(self):
+        if self.command_type[2:].lower not in {'push', 'pop', 'function', 'call'}:
+            return self.currCommand.split()[2]
+        else:
+            raise TypeError("Must be Push/Pop/Function/Call")
+    
+
+
+
+
+
+
+
+
+
